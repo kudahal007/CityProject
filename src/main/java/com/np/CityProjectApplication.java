aa188f7ce84a64ea620e150d1a010e0e9b5e7713
Kumar Dahal <kudahal007@gmail.com>
2024-09-13 12:43:37 -0400

    initializing spring boot project

---
+++ src/main/java/com/np/CityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.np;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityProjectApplication.class, args);
+	}
+
+}
